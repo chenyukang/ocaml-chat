fe0b8287a83170b840763cfcd5ebb437f7af8843
Yukang <moorekang@gmail.com>
2020-01-23 00:37:28 +0800

    fix server send

--- src/server.ml
+++ src/server.ml
@@ -1,5 +1,6 @@
 open Lwt
 open Logs
+open Core
 
 (* Shared mutable counter *)
 let counter = ref 0
@@ -12,6 +13,7 @@ let handle_message msg =
   | _      -> "Unknown command"
 
 let rec handle_connection_reply ic oc () =
+  let _ = Lwt_io.flush_all in
   Lwt_io.read_line_opt ic >>=
   (fun msg ->
      match msg with
@@ -22,24 +24,34 @@ let rec handle_connection_reply ic oc () =
      | None -> Logs_lwt.info (fun m -> m "Connection closed") >>= Lwt.return)
 
 let rec handle_connection_out oc () =
-  Lwt_io.read_line_opt Lwt_io.stdin >>=
-  (fun input ->
-     match input with
-     | Some str -> Lwt_io.write_line oc str >>= handle_connection_out oc
-     | None -> Logs_lwt.info (fun m -> m "No input from server" >>= Lwt.return))
+  while true do
+    print_string  "#=> ";
+    Out_channel.flush Out_channel.stdout;
+    let _ = match In_channel.input_line In_channel.stdin with
+    | Some(str) -> (
+        print_string  ("send=> " ^ str ^ "\n");
+        Out_channel.flush Out_channel.stdout;
+        let _ = Lwt_io.write_line oc str in
+        Lwt_io.flush_all() >>= Lwt.return
+      )
+    | _ -> Lwt_io.flush_all() >>= Lwt.return
+    in ()
+  done;
+  Logs_lwt.info (fun m -> m "No input from server" >>= Lwt.return)
 
 let accept_connection conn =
   let fd, _ = conn in
   let ic = Lwt_io.of_fd ~mode:Lwt_io.Input fd in
   let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
+  let log = Logs_lwt.info (fun m -> m "New connection") in
   let _ = match Unix.fork () with
-    | 0 ->
+    | `In_the_parent _ ->
         Lwt.on_failure (handle_connection_out oc ())
           (fun e -> Logs.err (fun m -> m "%s" (Caml.Printexc.to_string e)))
-    | pid -> (
+    | `In_the_child -> (
         Lwt.on_failure (handle_connection_reply ic oc ())
           (fun e -> Logs.err (fun m -> m "%s" (Caml.Printexc.to_string e)))) in
-  Logs_lwt.info (fun m -> m "New connection") >>= Lwt.return
+  log >>= Lwt.return
 
 let create_socket listen_address port =
   let open Lwt_unix in
